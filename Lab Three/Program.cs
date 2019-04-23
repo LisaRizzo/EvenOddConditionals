477d8de04ee9971e4935bdd49df69441f4b8c9ab
Lisa <lisa.a.bol@gmail.com>
2019-04-23 14:10:59 -0400

    updated loops

--- Lab Three/Program.cs
+++ Lab Three/Program.cs
@@ -15,15 +15,16 @@ namespace Lab_Three
 
       char doAgain;
       bool repeat1 = true;
-        Console.WriteLine(name + ", enter a number between 1 and 100.");
-        string input = Console.ReadLine();
-
       while (repeat1 == true)
       {
+        Console.WriteLine(name + ", enter a number between 1 and 100.");
+        string input = Console.ReadLine();
         // Use method for checking for a number.
+
         if (!IsAllDigits(input))
         {
-          Console.WriteLine("Hey! That isn't a number. Would you like to try again? Y or N");
+          Console.WriteLine("Hey! That isn't a number.");
+          Console.WriteLine("Would you like to try again? Y or N");
           doAgain = Convert.ToChar(Console.ReadLine());
 
           if (doAgain == 'y' || doAgain == 'Y')
@@ -35,58 +36,56 @@ namespace Lab_Three
             break;
           }
         }
+        int userInput = int.Parse(input);
+        if (userInput % 2 == 0)
+        {
+          if (userInput <= 25)
+
+          {
+            Console.WriteLine(name + ", your number is even and less than 25.");
+            break;
+          }
+
+          else if (userInput >= 26 || userInput <= 60)
+
+          {
+            Console.WriteLine(name + ", your number is even.");
+            break;
+          }
+
+          else if (userInput > 60)
+
+          {
+            Console.WriteLine(name + "," + userInput + " is an even number.");
+            break;
+          }
+        }
         else
         {
+          Console.WriteLine(name + "," + userInput + " is an odd number.");
           break;
         }
-      }
-      int userInput = int.Parse(input);
-      if (userInput % 2 == 0)
-      {
-        if (userInput <= 25)
 
+        // IsAllDigits Method
+        bool IsAllDigits(string raw)
         {
-          Console.WriteLine(name + " ,your number is even and less than 25.");
-        }
+          // First get rid of any benign characters at either end of string.
+          // If there is nothing left, no number.
+          string s = raw.Trim();
+          if (s.Length == 0) return false;
 
-        else if (userInput >= 26 || userInput <= 60)
-
-        {
-          Console.WriteLine(name + ", your number is even.");
-        }
-
-        else if (userInput > 60)
-
-        {
-          Console.WriteLine(name + "," + userInput + " is an even number.");
+          // Loop through the string.
+          for (int index = 0; index < s.Length; index++)
+          {
+            // A nondigit indicates that the string is not a number.
+            if (Char.IsDigit(s[index]) == false) return false;
+          }
+          // No nondigit is found; it's okay!
+          return true;
         }
       }
-      else
-      {
-        Console.WriteLine(name + "," + userInput + " is an odd number.");
-      }
-
       //Wait for user to acknowledge the results.
       Console.WriteLine("Bye!");
-
-      // IsAllDigits Method
-      bool IsAllDigits(string raw)
-      {
-        // First get rid of any benign characters at either end of string.
-        // If there is nothing left, no number.
-        string s = raw.Trim();
-        if (s.Length == 0) return false;
-
-        // Loop through the string.
-        for (int index = 0; index < s.Length; index++)
-        {
-          // A nondigit indicates that the string is not a number.
-          if (Char.IsDigit(s[index]) == false) return false;
-        }
-        // No nondigit is found; it's okay!
-        return true;
-      }
-
     }
-  }
+  } 
 }
